38d667c6206b0288563441f578392bb01191780e
StephenSant <42423953+StephenSant@users.noreply.github.com>
2018-10-20 20:19:32 +1100

    A new beginning

---
+++ Unity Practice/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Variables
+    public List<Item> inv = new List<Item>();
+    public static bool showInv;
+    public Item selectedItem;
+    public int money;
+
+    public Vector2 scr = Vector2.zero;
+    public Vector2 scrollPos = Vector2.zero;
+    #endregion
+    void Start()
+    {
+        inv.Add(ItemData.CreateItem(0));
+        inv.Add(ItemData.CreateItem(202));
+        inv.Add(ItemData.CreateItem(401));
+        inv.Add(ItemData.CreateItem(102));
+        inv.Add(ItemData.CreateItem(301));
+        inv.Add(ItemData.CreateItem(301));
+        inv.Add(ItemData.CreateItem(2));
+        inv.Add(ItemData.CreateItem(404));
+
+        for (int i = 0; i < inv.Count; i++)
+        {
+            Debug.Log(inv[i].Name);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleInv();
+        }
+    }
+    public bool ToggleInv()
+    {
+        if (showInv)
+        {
+            showInv = false;
+            Time.timeScale = 1;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            return (false);
+        }
+        else
+        {
+            showInv = true;
+            Time.timeScale = 0;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+            return (true);
+        }
+    }
+    private void OnGUI()
+    {
+        if (!PauseMenu.paused)
+        {
+            if (showInv)
+            {
+                if (scr.x != Screen.width / 16 || scr.y != Screen.height / 9)
+                {
+                    scr.x = Screen.width / 16;
+                    scr.y = Screen.height / 9;
+                }
+                GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "Inventory");
+                #region Non Scroll Inventory
+                if (inv.Count <= 35)
+                {
+                    for (int i = 0; i < inv.Count; i++)
+                    {
+                        if (GUI.Button(new Rect(0.5f * scr.x, 0.25f * scr.y + i * (0.25f * scr.y), 3 * scr.x, 0.25f * scr.y), inv[i].Name))
+                        {
+                            selectedItem = inv[i];
+                            Debug.Log(selectedItem.Name);
+                        }
+                    }
+                }
+                #endregion
+                #region Scroll Inventory
+
+                #endregion
+            }
+        }
+    }
+}
---
+++ Unity Practice/Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    #region Variables
+    public static bool paused;
+    public bool showOptions;
+    public GameObject player;
+    public GameObject optionsPanel;
+    public GameObject pauseMenu;
+    public Slider sensitivityY;
+    public Slider sensitivityX;
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        pauseMenu = GameObject.Find("Pause Menu");
+        optionsPanel = GameObject.Find("Pause Options Menu");
+        paused = false;
+        showOptions = false;
+        pauseMenu.SetActive(false);
+        optionsPanel.SetActive(false);
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("GUI Scene");
+    }
+
+    public void SenSliders()
+    {
+        player.GetComponent<CharacterMovement>().sensitivityX = sensitivityX.value;
+        player.GetComponent<CharacterMovement>().sensitivityY = sensitivityY.value;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !showOptions)
+        {
+            
+            TogglePause();
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && showOptions)
+        {
+            ToggleOptions();
+        }
+
+        if (paused)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+    public void TogglePause()
+    {
+        if (paused && !showOptions && !Inventory.showInv)
+        {
+            Time.timeScale = 1;
+            paused = false;
+            pauseMenu.SetActive(false);
+        }
+        else if (paused && showOptions)
+        {
+            ToggleOptions();
+            
+        }
+        else if (paused && !showOptions && Inventory.showInv)
+        {
+            paused = false;
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 0;
+            paused = true;
+            pauseMenu.SetActive(true);
+        }
+    }
+    public void ToggleOptions()
+    {
+        if (!showOptions)
+        {
+            pauseMenu.SetActive(false);
+            optionsPanel.SetActive(true);
+            showOptions = true;
+        }
+        else if (showOptions)
+        {
+            pauseMenu.SetActive(true);
+            optionsPanel.SetActive(false);
+            showOptions = false;
+        }
+    }
+}
+
